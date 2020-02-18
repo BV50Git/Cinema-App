677820470c35aeaf02c1c959e183245cf5dd4348
BV50Git <BV50Git@users.noreply.github.com>
2020-02-18 13:37:52 +0100

    I destroyed this piece of work.
    np

--- CInemaApp/Program.cs
+++ CInemaApp/Program.cs
@@ -1,12 +1,1 @@
-﻿using System;
-
-namespace CInemaApp
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
-}
+﻿hello world xd
