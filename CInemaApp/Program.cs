2b55ac511b436630b239636b71249319d03c3789
AmamiSQ <58980413+AmamiSQ@users.noreply.github.com>
2020-03-10 13:19:52 +0100

    hhuhh

--- CInemaApp/Program.cs
+++ CInemaApp/Program.cs
@@ -11,9 +11,16 @@ namespace CInemaApp
                 Console.WriteLine("teeesssttttt");
             }
 
+            static void part2()
+            {
+                Console.WriteLine("neeeeiiiiiinnnnnn");
+            }
+
             int inp = Convert.ToInt32(Console.ReadLine());
             if (inp > 0)
                 testdef();
+            else
+                part2();
         }
     }
 }
