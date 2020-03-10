68de4d4c9fc93c4e3c3f0982bd589e595312c915
BV50Git <BV50Git@users.noreply.github.com>
2020-03-10 14:35:58 +0100

    Seating reservation progress
    Shows all availible seats, lets user input the row and column of the seat they want to reserve, then shows all availible seats but change the O of the user's seat to an X.

--- CInemaApp/Program.cs
+++ CInemaApp/Program.cs
@@ -1,12 +1,75 @@
 ﻿using System;
-
-namespace CInemaApp
+namespace CinemaApp
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int width = 0;
+            int height = 0;
+            int movieHall = 1;
+            var userRow = 0;
+            var userColumn = 0;
+
+            if (movieHall == 1)
+            {
+                width = 15;
+                height = 10;
+            }
+            else if (movieHall == 2)
+            {
+                width = 20;
+                height = 15;
+            }
+            else if (movieHall == 3)
+            {
+                width = 25;
+                height = 20;
+            }
+
+            void showSeats()
+            {
+                for (int i = 0; i < height; i++)
+                {
+                    for (int o = 0; o < width; o++)
+                    {
+                        if (userRow == i + 1 && userColumn == o + 1)
+                        {
+                            Console.Write("X");
+                        }
+                        else
+                        {
+                            Console.Write("O");
+                        }
+                    }
+                    Console.Write("\n");
+                }
+            }
+            void seatSelection()
+            {
+                Console.WriteLine("Enter the row of the seat you want to reserve (1 - " + height + ")");
+                bool parseSucc = int.TryParse(Console.ReadLine(), out userRow);
+                if (parseSucc)
+                {
+                    Console.WriteLine(userRow);
+                }
+                Console.WriteLine("Enter the column of the seat you want to reserve (1 - " + width + ")");
+                bool parseSucc = int.TryParse(Console.ReadLine(), out userColumn);
+                if (parseSucc)
+                {
+                    Console.WriteLine(userColumn);
+                }
+                showSeats();
+            }
+            showSeats();
+            seatSelection();
+            Console.WriteLine("Type OK to confirm reservation, type anything else to select a different seat.");
+            while(Console.ReadLine() != "OK")
+            {
+                seatSelection();
+                Console.WriteLine("Type OK to confirm reservation, type anything else to select a different seat.");
+            }
         }
     }
+
 }
