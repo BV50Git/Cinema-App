2829cbbe31d3a7d3198854d7cd839fb4d615a03a
rowan-rinn <60717054+rowan-rinn@users.noreply.github.com>
2020-03-16 22:38:59 +0100

    code arrays prices based on age. I still need to make it so these are callable functions. first part is code that allows a administrator to change the prices. second part shows the prices and the age bracket.

--- CInemaApp/Program.cs
+++ CInemaApp/Program.cs
@@ -6,7 +6,21 @@ namespace CInemaApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("in the jungle!");
+            // function to let the administrator change values, i do not yet know how to make a function in c#
+            string[] agebrackets_in_cinema = { "below age 10", "age 10 to 15", "age 16 to 26", "age 27 to 50", "50+"};
+            string[] array = { "1", "2", "3", "4", "5" };
+            int agebrackets = 5;
+            for (int i = 0; i < agebrackets; i++){
+
+                string agebracket_and_price = Console.ReadLine();
+                array[i] = agebrackets_in_cinema[i] + " " + agebracket_and_price;
+            }
+            // function to show the lists of prices, I do not yet know how to do that in c#
+            Console.WriteLine(array[0]);
+            Console.WriteLine(array[1]);
+            Console.WriteLine(array[2]);
+            Console.WriteLine(array[3]);
+            Console.WriteLine(array[4]);
         }
     }
 }
