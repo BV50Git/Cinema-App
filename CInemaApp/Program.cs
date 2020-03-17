d40caaeee24ee3dd34c945934dee66602b0ece0f
rowan-rinn <60717054+rowan-rinn@users.noreply.github.com>
2020-03-17 07:57:21 +0100

    new functionality for movie time/date/location. needs to be put in functions just like before needs to be put in while loop

--- CInemaApp/Program.cs
+++ CInemaApp/Program.cs
@@ -21,6 +21,26 @@ namespace CInemaApp
             Console.WriteLine(array[2]);
             Console.WriteLine(array[3]);
             Console.WriteLine(array[4]);
+
+            // function to change time/date/location movies will be played at.
+            // this function needs allot more work, mostly on the design.
+            string location = "locatie: Rotterdam, Wijnhaven 107.";
+            var array_movie_times_and_location = new System.Collections.Generic.List<string>() {};
+            Console.WriteLine("type 1 to add specifics for a movie, anything else to leave");
+            string a = Console.ReadLine();
+            if (a == "1")
+            {
+                Console.WriteLine("At what time does the movie play!");
+                string time = Console.ReadLine();
+                Console.WriteLine("At which date does the movie play!");
+                string date = Console.ReadLine();
+                string newdata = "movie starts at " + time + " on " + date + " " + location;
+                array_movie_times_and_location.Add(newdata);
+                Console.WriteLine(array_movie_times_and_location[0]);
+            }
+            else if (a != "1"){
+                Console.WriteLine(array_movie_times_and_location);
+            } 
         }
     }
 }
