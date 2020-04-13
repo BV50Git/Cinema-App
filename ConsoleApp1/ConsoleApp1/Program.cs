d07928fe409c3fb8c0a366c6086ea4a1b690d478
nbxromy <61189931+nbxromy@users.noreply.github.com>
2020-04-13 17:47:32 +0200

    Hii

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -6,7 +6,17 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!  test test");
+            Console.WriteLine("These are the popularmovies: ");
+            List<string> Movies = new List<string>();
+            Movies.Add("Bad Boys for Life from 2020");
+            Movies.Add("The Godfather from 1974");
+            Movies.Add("1917 from 2020");
+            Movies.Add("Guardians of the Galaxy from 2014");
+            Movies.Add("Joker from 2019");
+
+            foreach (var movie in Movies)
+
+                Console.WriteLine(Movies);
         }
     }
 }
