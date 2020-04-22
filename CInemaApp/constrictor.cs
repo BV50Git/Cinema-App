324d702434cf960691e48fc982b97e233c39aa7c
jeroen107 <38460108+jeroen107@users.noreply.github.com>
2020-04-22 17:59:59 +0200

    test

---
+++ CInemaApp/constrictor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+/*public class Constructor
+ * used for future conversion json file
+{
+    public string movietimes;
+    public string prices;
+
+    public void Timeandprices(string movietimes, string prices)
+	{
+        this.movietimes = movietimes;
+        this.prices = prices;
+	}
+    public string Getmovietime()
+    {
+        return this.prices;
+    }
+}
+*/
