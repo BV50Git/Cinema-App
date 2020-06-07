611a9e1b162507cbb30d2ccc49ccf9b97720b272
rowan-rinn <60717054+rowan-rinn@users.noreply.github.com>
2020-06-07 19:50:07 +0200

    okay we nee to talk after this for last changes

--- CInemaApp/Program.cs
+++ CInemaApp/Program.cs
@@ -451,7 +451,7 @@ namespace CInemaApp
                     FoodDrinkMenu();
                     break;
                 case "2":
-                    PaymentOption();
+                PaymentOptionsally();
                     break;
                 case "3":
                     OrderMenu();
@@ -507,68 +507,11 @@ namespace CInemaApp
 
         public static void PaymentOptionsally()
         {
-            string c = "0";
-            string s = "";
-            int counter = 1;
-            int options = 2;
-            int i;
-            int j;
-            int height = 9;
-            int width = 9;
-            while (counter < options)
-            {
-                for (i = 1; i <= height; i++)
-                {
-                    for (j = 1; j <= width; j++)
-                    {
-                        if (i == 1 || i == height || j == 1 || j == width)
-                            s = s + " *";
-                        else
-                            s = s + "  ";
-                    }
-                    s = s + "\n";
-                }
-                if (counter == 1)
-                {
-                    Console.WriteLine(s);
-                    Console.WriteLine("   IDEAL option 1");
-                }
-                else if (counter == 2)
-                {
-                    Console.WriteLine(s);
-                    Console.WriteLine(" Credit Card option 2");
-                }
-                counter = counter + 1;
-            }
+        Console.WriteLine("These are the following payment options\n");
+        Console.WriteLine("1) IDEAL");
+        Console.WriteLine("2) Creditcard");
 
-            Console.WriteLine(s);
-            Console.WriteLine(" Credit Card option 2");
-            while (c != "4")
-            {
-                c = Console.ReadLine();
-                if (c == "1")
-                {
-                    Console.WriteLine("Please enter your IDEAL information");
-                    User.STARS();
-                    string information = Console.ReadLine();
-                    string x = "5";
-                    c = "4";
-                }
-                if (c == "2")
-                {
-                    Console.WriteLine("Please enter your Credit Card information");
-                    User.STARS();
-                    string information = Console.ReadLine();
-
-                    c = "4";
-                }
-
-
-            }
-            // shows final ticket and show the time and payment done.
-            string infopayed = " Payment completed";
-
-        }
+    }
 
         public static void OrderMenu()
         {
